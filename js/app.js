ec92fa854c058f22f1ea86db40290343112a185c
m2creger <marshall.creger@gmail.com>
2017-07-13 22:48:22 -0600

    configured results html elements

--- js/app.js
+++ js/app.js
@@ -51,7 +51,8 @@ window.onload = function() {
 }
 function submitGameResults() {
 	var submit = document.getElementById('submitGame');
-	console.log(submit);
+	category1 = document.getElementById("category1Text").value;
+	console.log(category1);
 }
 function clearRound() {
 	clearInterval(interval);
@@ -66,12 +67,14 @@ var player1words = [];
 var player2words = [];
 var categories = ["sports", "U.S. cities", "gemstone", "vegetable", "a tool", "a zoo animal", "a fast food item", "an item found inside of a refrigerator", "a type of footwear", "a musical instrument", "something you would find in a garage", "item from a catalog", "a school supply item", "item you may find in an attic", "a kitchen appliance", "a type or item of clothing", "a type of drink or beverage"];
 var letters = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, R, S, T, W"];
-var category1 = document.getElementById("category1Text").value;
-var category2 = document.getElementById("category2Text").value;
-var category3 = document.getElementById("category3Text").value;
-var category4 = document.getElementById("category4Text").value;
-var category5 = document.getElementById("category5Text").value;
-var category6 = document.getElementById("category6Text").value;
+var category1; 
+/* 
+var category2; = document.getElementById("category2Text").value;
+var category3; = document.getElementById("category3Text").value;
+var category4; = document.getElementById("category4Text").value;
+var category5; = document.getElementById("category5Text").value;
+var category6; = document.getElementById("category6Text").value;
+*/ 
 var timer = document.getElementById("timerDigits");
 
 function getDictionaryData() {
