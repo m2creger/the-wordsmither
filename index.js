a0995f203e7a36b326bf20e2aedfceb5161e1322
m2creger <marshall.creger@gmail.com>
2017-07-13 18:17:12 -0600

    changed networking aspects

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
 /*
 function hostCreateNewGame() {
 	console.log("new game created")
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("load", function(getDictionaryData) {
 	console.log("All resources finished loading!");
 })
